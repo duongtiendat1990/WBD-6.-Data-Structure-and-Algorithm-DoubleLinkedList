29ae194ad38a5d551a322c4892830dcaed7292b4
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-05-13 16:05:50 +0700

    Add tail property

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -2,12 +2,11 @@
 public class MyLinkedList {
   private Node head;
   private Node tail;
-  private  int numNodes;
+  private int numNodes;
+
   public MyLinkedList(Object data) {
     head = new Node(data);
-    tail = new Node(data);
-    head.next = tail;
-    tail.next = null;
+    tail = head;
     numNodes++;
   }
 
@@ -23,38 +22,50 @@ public class MyLinkedList {
       return this.data;
     }
   }
-  public void add(int index, Object data) {
-    Node temp = head;
-    Node holder;
 
-    for(int i=0; i < index-1 && temp.next != null; i++) {
-      temp = temp.next;
+  public void add(int index, Object data) {
+    if (index <= numNodes - 1 && index >= 0) {
+      if (index == 0) addFirst(data);
+      else if (index == numNodes - 1) addLast(data);
+      else {
+        Node temp = head;
+        Node holder;
+
+        for (int i = 1; i < index - 1 && temp.next != tail; i++) {
+          temp = temp.next;
+        }
+        holder = temp.next;
+        temp.next = new Node(data);
+        temp.next.next = holder;
+        numNodes++;
+      }
     }
-    holder = temp.next;
-    temp.next = new Node(data);
-    temp.next.next = holder;
-    numNodes++;
   }
+
   public void addFirst(Object data) {
     Node temp = head;
     head = new Node(data);
     head.next = temp;
     numNodes++;
   }
+
   public void addLast(Object data) {
     tail.next = new Node(data);
+    tail = tail.next;
     numNodes++;
   }
-  public Node get(int index){
-    Node temp=head;
-    for(int i=0; i<index; i++) {
+
+  public Node get(int index) {
+    Node temp = head;
+    for (int i = 0; i < index; i++) {
       temp = temp.next;
     }
     return temp;
   }
+
   public void printList() {
     Node temp = head;
-    while(temp != null) {
+    while (temp != null) {
       System.out.println(temp.data);
       temp = temp.next;
     }
--- src/TestMyLinkedList.java
+++ src/TestMyLinkedList.java
@@ -3,12 +3,13 @@ public class TestMyLinkedList {
     System.out.println("/=/=/=/= TESTING /=/=/=/=");
     MyLinkedList ll = new MyLinkedList(9);
     ll.addFirst(11);
-    ll.addFirst(12);
-    ll.addFirst(13);
-
-    ll.add(4,9);
-    ll.add(4,8);
-    ll.addLast(100);
+//    ll.addFirst(12);
+//    ll.addFirst(13);
+//
+//    ll.add(4,9);
+//    ll.addLast(100);
+    ll.add(-1,8);
+    ll.addLast(110);
     ll.printList();
   }
 }
