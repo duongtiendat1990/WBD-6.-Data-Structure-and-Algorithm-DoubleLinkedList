c9aaf9e04befd3117f1a80d262fd0d28fee1552e
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-05-13 17:15:07 +0700

    Construct with new array

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,7 +1,7 @@
 
 public class MyLinkedList {
-  private Node head;
-  private Node tail;
+  protected Node head;
+  protected Node tail;
   private int numNodes;
 
   public MyLinkedList(Object data) {
@@ -10,7 +10,16 @@ public class MyLinkedList {
     numNodes++;
   }
 
-  private class Node {
+  public MyLinkedList (Object ...array){
+    head = new Node(array[0]);
+    tail = new Node(array[array.length-1]);
+    Node current = head;
+    for (int i = 1; i<array.length;i++){
+      current.next = new Node(array[i]);
+      current = current.next;
+    }
+  }
+  protected class Node {
     private Node next;
     private Object data;
 
@@ -24,19 +33,19 @@ public class MyLinkedList {
   }
 
   public void add(int index, Object data) {
-    if (index <= numNodes - 1 && index >= 0) {
+    if (index <= numNodes  && index >= 0) {
       if (index == 0) addFirst(data);
-      else if (index == numNodes - 1) addLast(data);
+      else if (index == numNodes ) addLast(data);
       else {
-        Node temp = head;
-        Node holder;
+        Node current = head;
+        Node temp;
 
-        for (int i = 1; i < index - 1 && temp.next != tail; i++) {
-          temp = temp.next;
+        for (int i = 1; i < index ; i++) {
+          current = current.next;
         }
-        holder = temp.next;
-        temp.next = new Node(data);
-        temp.next.next = holder;
+        temp = current.next;
+        current.next = new Node(data);
+        current.next.next = temp;
         numNodes++;
       }
     }
--- src/TestMyLinkedList.java
+++ src/TestMyLinkedList.java
@@ -1,15 +1,17 @@
 public class TestMyLinkedList {
   public static void main(String[] args) {
     System.out.println("/=/=/=/= TESTING /=/=/=/=");
-    MyLinkedList ll = new MyLinkedList(9);
+    MyLinkedList ll = new MyLinkedList(1,2,3,1,5,1,6,12);
     ll.addFirst(11);
-//    ll.addFirst(12);
-//    ll.addFirst(13);
-//
-//    ll.add(4,9);
-//    ll.addLast(100);
-    ll.add(-1,8);
+    ll.addFirst(12);
+    ll.addFirst(13);
+
+    ll.add(3,8);
+    ll.addLast(100);
+    ll.add(2,8);
     ll.addLast(110);
     ll.printList();
+    System.out.println("Head" + ll.head.getData());
+    System.out.println("Tail" + ll.tail.getData());
   }
 }
